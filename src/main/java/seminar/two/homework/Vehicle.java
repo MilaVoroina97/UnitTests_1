a7a4f9332d472e83cec32d83a0382669abe20689
mila <milavoronina1997@mail.ru>
2023-10-27 21:56:54 +0500

    change structure and create classes Car,Vehicle,Motorcycle

--- src/main/java/seminar/two/homework/Vehicle.java
+++ src/main/java/seminar/two/homework/Vehicle.java
@@ -1,4 +1,13 @@
 package seminar.two.homework;
 
-public class Vehicle {
+public abstract class Vehicle {
+    private String company;
+    private String model;
+    private int yearRelease;
+    private int numWheels;
+    private int speed;
+
+    public abstract void testDrive();
+
+    public abstract void park();
 }
