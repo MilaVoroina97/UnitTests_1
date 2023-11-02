c2f755a633db489d059b05914ba9d32cbbdf0c7d
mila <milavoronina1997@mail.ru>
2023-11-02 12:38:38 +0500

    correct test class

--- src/test/java/seminarFour/BookServiceTest.java
+++ src/test/java/seminarFour/BookServiceTest.java
@@ -16,15 +16,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-// Подключаем расширение Mockito к тестируемому классу
+// Подключение расширения Mockito к тестируемому классу
 @ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
 
-    // Создаем заглушку типа Mock для интерфейса BookRepository
+    /* 1. Создаем mock -заглушку для интерфейса BookRepository
+    * */
     @Mock
-    private BookRepository mockBockService;
+    private BookRepository mockBockRepository;
 
-    // Внедряем мокируемую заглушку в конструктор класса BookService
+    /* 2. Внедряем  заглушку в конструктор класса BookService
+    * */
     @InjectMocks
     private BookService bookService;
 
@@ -33,26 +35,23 @@ public class BookServiceTest {
      */
     @Test
     public void findBookByIdShouldReturnBook() {
-        /* Задаем поведение заглушки:
-            когда будет вызван метод findById() с аргументом 1,
-            данный метод должен вернуть книгу с:
-                id = 1;
-                title = SomeTitle;
-                author = SomeAuthor;
-         */
-        when(mockBockService.findById("1")).thenReturn(new Book("1", "SomeTitle", "SomeAuthor"));
 
-        /* Создаем ожидаемый id и получаем id книги при вызове метода findBookById() класса BookService.
-            Примечание: данные операции можно опустить если реализовать методы equals() и hashCode() в классе Book
-                        и провести сравнение ожидаемого и получаемого объекта.
-         */
+        when(mockBockRepository.findById("1")).thenReturn(new Book("1", "SomeTitle", "SomeAuthor"));
+
+        /* 3. Создаем ожидаемый id и получаем id книги при вызове метода findBookById() класса BookService.
+        * */
         String expectIdBook = "1";
         String actualIdBook = bookService.findBookById("1").getId();
 
-        // Проверяем что у заглушки один раз был вызван метод findById() с аргументом 1
-        verify(mockBockService).findById("1");
-        // Проверяем что ожидаемый id равен id полученного объекта
-        assertEquals(expectIdBook, actualIdBook, "Метод findBookById возвращает объект с не корректным ID");
+        /* 4. Проверяем что у заглушки один раз был вызван метод findById() с аргументом 1
+        * */
+
+        verify(mockBockRepository).findById("1");
+
+        /* 5. Проверяем что ожидаемый id равен id полученного объекта
+        * */
+
+        assertEquals(expectIdBook, actualIdBook, "Метод findBookById возвращает объект с некорректным ID");
     }
 
     /**
@@ -60,24 +59,29 @@ public class BookServiceTest {
      */
     @Test
     public void findAllBooksShouldReturnListBooks() {
-        /* Задаем поведение заглушки:
-            когда будет вызван метод findAllBooks(),
-            данный метод должен вернуть список с 4 книгами
-         */
-        when(mockBockService.findAll())
+
+        when(mockBockRepository.findAll())
                 .thenReturn(new ArrayList<>(Arrays.asList(
                         new Book("1"),
                         new Book("2"),
                         new Book("3"),
                         new Book("4")
                 )));
-        // Задаем ожидаемый размер листа книг и получаем размер листа при вызове метода findAllBooks класса BookService
+
+        /* 6. Задаем ожидаемый размер листа книг и получаем размер листа при вызове метода findAllBooks класса BookService
+        * */
+
         int expectedSizeListBooks = 4;
         int actualSizeListBooks = bookService.findAllBooks().size();
 
-        // Проверяем что у заглушки один раз был вызван метод findAll()
-        verify(mockBockService).findAll();
-        // Проверяем что размер листа равен полученному
+        /* 7. Проверяем что у заглушки один раз был вызван метод findAll()
+        * */
+
+        verify(mockBockRepository).findAll();
+
+        /* 8. Проверяем что размер листа равен полученному
+        * */
+
         assertEquals(expectedSizeListBooks,
                 actualSizeListBooks,
                 "Метод findAllBooks возвращает некорректный размер листа с книгами");
